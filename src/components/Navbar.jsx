0a6861b7962c1521c3f8d03ab40b3f7727f7dc5d
Nurkahfi Amran Rahmada <nurkahfirahmada@gmail.com>
2025-08-30 19:44:07 +0700

    feat: new navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,18 +3,17 @@ import ThemeToggle from "./ThemeToggle"
 export default function Navbar()
 {
     return (
-        <div className="w-full flex py-4 justify-between items-center">
-            <div className="italic font-black dark:text-white text-black">
+        <div className="w-full flex py-6 md:px-20 text-xl justify-between items-center">
+            <div className="italic font-black">
                 KV.
             </div>
 
-            {/* <div className="flex gap-8">
-                <p className="hover:underline cursor-pointer">Skills</p>
-                <p className="hover:underline cursor-pointer">About</p>
-                <p className="hover:underline cursor-pointer">Projects</p>
-            </div> */}
-
-            <ThemeToggle/>
+            <div className="flex gap-8">
+                <p className="md:block hidden">Work</p>
+                <p className="md:block hidden">About</p>
+                <p className="md:block hidden">Contact</p>
+                <ThemeToggle/>
+            </div>
         </div>
     )
 }
