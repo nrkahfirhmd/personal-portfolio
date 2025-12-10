6c54e1d990ca679054131ce9476881a37fe3b27b
Nurkahfi Amran Rahmada <nurkahfirahmada@gmail.com>
2025-12-10 22:03:26 +0700

    build: reset all

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,10 @@
-import { StrictMode, useState } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import Cursor from './components/Cursor'
-import { ContactProvider } from './context/contactContext'
-
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div className='w-full flex justify-center h-fit dark:bg-[var(--dark-bg)] bg-[var(--light-bg)] dark:text-[var(--dark-fg)] text-[var(--light-fg)]'>
-      <div className='w-full flex flex-col'>
-        <ContactProvider>
-          <Navbar/>
-          <App />
-          <Footer/>
-          <Cursor/>
-        </ContactProvider>
-      </div>
-    </div>
-  </StrictMode>
+    <App />
+  </StrictMode>,
 )
