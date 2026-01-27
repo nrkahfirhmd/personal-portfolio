96b5bff44f753afc652cae3c21e7bf90e58aa2de
Nurkahfi Amran Rahmada <nurkahfirahmada@gmail.com>
2026-01-27 17:18:37 +0700

    build: reset all

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,35 @@
-import { Route, Routes } from "react-router-dom";
-import Sidebar from "./components/Sidebar";
-import Hero from "./pages/Hero";
-import About from "./pages/About";
-import Creation from "./pages/Creation";
-import MatrixBackground from "./components/MatrixBackground";
-import Resume from "./pages/Resume";
-import ThemeModal from "./components/ThemeModal";
-import Chat from "./pages/Chat";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-export default function App() {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="h-dvh flex flex-col lg:flex-row">
-      <MatrixBackground/>
-      <ThemeModal/>
-      <Sidebar/>
-      <div className="flex-1 p-8 flex justify-center items-center">
-        <Routes>
-          <Route path="/" element={<Hero/>} />
-          <Route path="/experience" element={<About/>} />
-          <Route path="/creation" element={<Creation/>} />
-          <Route path="/resume" element={<Resume/>} />
-          <Route path="/chat" element={<Chat/>} />
-        </Routes>
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-    </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
+
+export default App
